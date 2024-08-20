424eb047bba9e220792366f5c6f5c96ab62c14fb
ifshivam <shivamrai0307@gmail.com>
2024-08-20 19:56:37 +0530

    creates bean of UserDetailsService

--- src/main/java/com/learning/spring_security/config/SecurityConfiguration.java
+++ src/main/java/com/learning/spring_security/config/SecurityConfiguration.java
@@ -3,6 +3,10 @@ package com.learning.spring_security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +23,11 @@ public class SecurityConfiguration {
        // http.formLogin(flc->flc.disable());//if you want to disable the form login
         http.httpBasic(withDefaults());
         return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withUsername("shivam").password("{noop}shivam@").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("read").build();
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
