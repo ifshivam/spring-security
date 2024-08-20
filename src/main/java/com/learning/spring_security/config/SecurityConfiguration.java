65859071d95744f7fd45ef9b183d69b6bd9a01ee
ifshivam <shivamrai0307@gmail.com>
2024-08-20 20:15:49 +0530

    creates bean of PasswordEncoder

--- src/main/java/com/learning/spring_security/config/SecurityConfiguration.java
+++ src/main/java/com/learning/spring_security/config/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,8 +28,14 @@ public class SecurityConfiguration {
     }
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("shivam").password("{noop}shivam@").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("read").build();
+       // UserDetails user = User.withUsername("shivam").password("{noop}shivam@").authorities("read").build();// when you don't use password encoder
+        UserDetails user = User.withUsername("shivam").password("{bcrypt}$2a$12$X0eAoo.bopi2KTbbfIfhDuP9bjv3bwKOAAdXHQtROAee.08uVOIP6").authorities("read").build(); // using passwordencoder
+        UserDetails admin = User.withUsername("admin").password("{noop}12345").authorities("admin").build();
         return new InMemoryUserDetailsManager(user,admin);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     }
 }
