5a295fb90b4fcccc1a605591652b41d30b07d702
ifshivam <shivamrai0307@gmail.com>
2024-08-22 02:32:59 +0530

    creates customer entity

---
+++ src/main/java/com/learning/spring_security/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.learning.spring_security.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String pwd;
+    private String role;
+
+}
